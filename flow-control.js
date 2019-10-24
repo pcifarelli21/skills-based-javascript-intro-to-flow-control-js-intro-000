da2fd8a3720e2b75efacd476be15bb4bf1e193b5
ambitious-cyborg-1541 <pcifarelli21@gmail.com>
2019-10-24 17:32:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,12 @@
 function basicTeenager(age) {
-  
+
   return "You are a "
 }
 
 function teenager(age) {
   if (age >= 13 && <= 19) {
   return "You are a teenager!"
-} 
+}
   else {
     return "You are not a teenager"
   }
